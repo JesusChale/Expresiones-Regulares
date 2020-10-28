13e15f663a9b8e3230823260dc37ad1188b8928d
JesusChale <72233199+JesusChale@users.noreply.github.com>
2020-10-27 20:18:00 -0500

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,32 @@
+function validacion() {
+    var valorPlaca = document.getElementById("placaAuto").value;
+    var valorNum = document.getElementById("numMiles").value;
+    var result1 = false;
+    var result2 = false;
+
+
+    if (!(/^((A(0[1-9])|A([1-9][0-9]))|([B-Z][0-9]{2}))-([A-Z]{3})$/.test(valorPlaca))) {
+        document.getElementById("errorPlaca").innerHTML = "No es una Placa valida";
+        document.getElementById("errorPlaca").style.color = "red";
+        result1 = false;
+    } else {
+        document.getElementById("errorPlaca").innerHTML = "Correcto";
+        document.getElementById("errorPlaca").style.color = "green";
+        result1 = true
+    }
+    if (!(/^([1-9]([0-9]{0,2}))(,([0-9]{3}))*(\.[0-9]{1,3})?$/.test(valorNum))) {
+        document.getElementById("errorMiles").innerHTML = "No es un numero valido";
+        document.getElementById("errorMiles").style.color = "red";
+        result2 = false;
+    } else {
+        document.getElementById("errorMiles").innerHTML = "Correcto";
+        document.getElementById("errorMiles").style.color = "green";
+        result2 = true;
+    }
+    if (result1 && result2) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
